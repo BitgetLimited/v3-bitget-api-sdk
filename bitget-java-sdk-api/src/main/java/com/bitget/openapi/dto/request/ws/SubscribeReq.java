a24a462a1d0c3d3c7cda83f06cec9a8dd35de5d1
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-09-24 11:54:54 +0800

    Create SubscribeReq.java
    add java websocket sdk

---
+++ bitget-java-sdk-api/src/main/java/com/bitget/openapi/dto/request/ws/SubscribeReq.java
@@ -0,0 +1,29 @@
+package com.bitget.openapi.dto.request.ws;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscribeReq {
+    private String instType;
+    private String channel;
+    private String instId;
+
+    @Override
+    public boolean equals(Object o) {
+        SubscribeReq that = (SubscribeReq) o;
+        return Objects.equals(instType.toUpperCase(), that.instType.toUpperCase()) && Objects.equals(channel.toUpperCase(), that.channel.toUpperCase()) && Objects.equals(instId.toUpperCase(), that.instId.toUpperCase());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instType.toUpperCase(), channel.toUpperCase(), instId.toUpperCase());
+    }
+}
