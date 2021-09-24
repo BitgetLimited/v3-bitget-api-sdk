abadda409d5b10c4a2d32da27d1d33ee44f18806
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-09-24 11:55:32 +0800

    Create BitgetWsClientTest.java
    add java websocket sdk

---
+++ bitget-java-sdk-api/src/test/java/com/bitget/openapi/ws/BitgetWsClientTest.java
@@ -0,0 +1,53 @@
+package com.bitget.openapi.ws;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.bitget.openapi.dto.request.ws.SubscribeReq;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BitgetWsClientTest {
+
+    public static final String PUSH_URL = "wss://ws.bitgetapi.com/spot/v1/stream";
+    public static final String API_KEY = "";
+    public static final String SECRET_KEY = "";
+    public static final String PASS_PHRASE = "";
+
+
+    public static void main(String[] args) {
+        BitgetWsClient client = BitgetWsHandle.builder()
+                                .pushUrl(PUSH_URL)
+                                .apiKey(API_KEY)
+                                .secretKey(SECRET_KEY)
+                                .passPhrase(PASS_PHRASE)
+                                .isLogin(true)
+                                //默认监听处理，如订阅时指定监听，默认不再接收该channel订阅信息
+                                .listener(response -> {
+                                    JSONObject json = JSONObject.parseObject(response);
+                                    System.out.println("def:" + json);
+                                //失败消息的逻辑处理,如:订阅失败
+                                }).errorListener(response -> {
+                                    JSONObject json = JSONObject.parseObject(response);
+                                    System.out.println("error:" + json);
+                                }).build();
+
+
+        List<SubscribeReq> list = new ArrayList<SubscribeReq>() {{
+            add(SubscribeReq.builder().instType("SP").channel("trade").instId("BTCUSDT").build());
+        }};
+        client.subscribe(list);
+
+        List<SubscribeReq> list2 = new ArrayList<SubscribeReq>() {{
+            add(SubscribeReq.builder().instType("SP").channel("trade").instId("BTCUSDT").build());
+        }};
+        client.subscribe(list2, response -> {
+            JSONObject json = JSONObject.parseObject(response);
+            System.out.println("appoint:" + json);
+        });
+    }
+
+}
