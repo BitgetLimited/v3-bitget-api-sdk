a55a0c52876f870fa7d00c754c2feb8526988c6a
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-09-24 11:55:22 +0800

    Create BitgetWsClient.java
    add java websocket sdk

---
+++ bitget-java-sdk-api/src/main/java/com/bitget/openapi/ws/BitgetWsClient.java
@@ -0,0 +1,21 @@
+package com.bitget.openapi.ws;
+
+import com.bitget.openapi.dto.request.ws.SubscribeReq;
+import com.bitget.openapi.dto.request.ws.WsBaseReq;
+
+import java.util.List;
+
+public interface BitgetWsClient {
+
+    void sendMessage(WsBaseReq<?> req);
+
+    void sendMessage(String message);
+
+    void unsubscribe(List<SubscribeReq> str);
+
+    void subscribe(List<SubscribeReq> list);
+
+    void subscribe(List<SubscribeReq> list,SubscriptionListener listener);
+
+    void login();
+}
