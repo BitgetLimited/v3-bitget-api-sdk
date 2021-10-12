ea1e93aad5cc0004c8614797e8880a90c12c32f4
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-10-12 11:02:06 +0800

    Create constants.go
    增加golang sdk

---
+++ bitget-golang-sdk-api/constants/constants.go
@@ -0,0 +1,59 @@
+package constants
+
+const (
+	/*
+	  http headers
+	*/
+	ContentType        = "Content-Type"
+	BgAccessKey        = "ACCESS-KEY"
+	BgAccessSign       = "ACCESS-SIGN"
+	BgAccessTimestamp  = "ACCESS-TIMESTAMP"
+	BgAccessPassphrase = "ACCESS-PASSPHRASE"
+	ApplicationJson    = "application/json"
+
+	EN_US  = "en_US"
+	ZH_CN  = "zh_CN"
+	LOCALE = "locale="
+
+	/*
+	  http methods
+	*/
+	GET    = "GET"
+	POST   = "POST"
+	DELETE = "DELETE"
+
+	/*
+	 others
+	*/
+	ResultDataJsonString = "resultDataJsonString"
+	ResultPageJsonString = "resultPageJsonString"
+
+	/**
+	 * SPOT URL
+	 */
+	SpotPublic  = "/api/spot/v1/public"
+	SpotMarket  = "/api/spot/v1/market"
+	SpotAccount = "/api/spot/v1/account"
+	SpotTrade   = "/api/spot/v1/trade"
+
+	/**
+	 * MIX URL
+	 */
+	MixPlan     = "/api/mix/v1/plan"
+	MixMarket   = "/api/mix/v1/market"
+	MixAccount  = "/api/mix/v1/account"
+	MixOrder    = "/api/mix/v1/order"
+	MixPosition = "/api/mix/v1/position"
+	MixTrace    = "/api/mix/v1/trace"
+
+	/**
+	websocket
+	*/
+	WsAuthMethod        = "GET"
+	WsAuthPath          = "/user/verify"
+	WsOpLogin           = "login"
+	WsOpUnsubscribe     = "unsubscribe"
+	WsOpSubscribe       = "subscribe"
+	TimerIntervalSecond = 5
+	ReconnectWaitSecond = 60
+)
