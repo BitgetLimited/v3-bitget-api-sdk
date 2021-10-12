4814b304c90e2bd646feade8f44c66851f41af79
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-10-12 11:03:58 +0800

    Create bitgetwsclient_test.go
    增加golang sdk

---
+++ bitget-golang-sdk-api/pkg/client/ws/bitgetwsclient_test.go
@@ -0,0 +1,38 @@
+package ws
+
+import (
+	"bitget/internal/model"
+	"fmt"
+	"testing"
+)
+
+func TestBitgetWsClient_New(t *testing.T) {
+
+	client := new(BitgetWsClient).Init(true, func(message string) {
+		fmt.Println("default error:" + message)
+	}, func(message string) {
+		fmt.Println("default error:" + message)
+	})
+
+	var channelsDef []model.SubscribeReq
+	subReqDef1 := model.SubscribeReq{
+		InstType: "UMCBL",
+		Channel:  "account",
+		InstId:   "default",
+	}
+	channelsDef = append(channelsDef, subReqDef1)
+	client.SubscribeDef(channelsDef)
+
+	var channels []model.SubscribeReq
+	subReq1 := model.SubscribeReq{
+		InstType: "UMCBL",
+		Channel:  "account",
+		InstId:   "default",
+	}
+	channels = append(channels, subReq1)
+	client.Subscribe(channels, func(message string) {
+		fmt.Println("appoint:" + message)
+	})
+	client.Connect()
+
+}
