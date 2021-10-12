34764f2975aca80acf8ab944307465bb647d20cb
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-10-12 11:02:26 +0800

    Create bitgetwsclient_test.go
    增加golang sdk

---
+++ bitget-golang-sdk-api/internal/common/bitgetwsclient_test.go
@@ -0,0 +1,13 @@
+package common
+
+import (
+	"testing"
+)
+
+func TestBitgetWsClient_Init(t *testing.T) {
+	//client := new(BitgetWsClient).Init(true)
+	//
+	//client.Connect()
+	////client.Login()
+	//fmt.Println("aaaaaaaaaa")
+}
