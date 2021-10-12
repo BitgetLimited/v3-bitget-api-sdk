4a7cab45cf2184d6e0af32fb241fbbcc680e4781
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-10-12 11:03:34 +0800

    Create spotaccountclient_test.go
    增加golang sdk

---
+++ bitget-golang-sdk-api/pkg/client/spot/spotaccountclient_test.go
@@ -0,0 +1,42 @@
+package spot
+
+import (
+	"bitget/pkg/model/spot/account"
+	"fmt"
+	"testing"
+)
+
+func TestSpotAccountClient_Assets(t *testing.T) {
+	client := new(SpotAccountClient).Init()
+
+	resp, err := client.Assets()
+
+	if err != nil {
+		println(err.Error())
+	}
+	fmt.Println(resp)
+}
+
+func TestSpotAccountClient_Bills(t *testing.T) {
+	client := new(SpotAccountClient).Init()
+
+	req := account.BillsReq{CoinId: "1", Before: "777031099461570560"}
+
+	resp, err := client.Bills(req)
+
+	if err != nil {
+		println(err.Error())
+	}
+	fmt.Println(resp)
+}
+
+func TestSpotAccountClient_TransferRecords(t *testing.T) {
+	client := new(SpotAccountClient).Init()
+
+	resp, err := client.TransferRecords("2", "USDT_MIX", "10", "", "")
+
+	if err != nil {
+		println(err.Error())
+	}
+	fmt.Println(resp)
+}
