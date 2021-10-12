02bdcc5ad27e02c80baccc4ad4a309cf6c8e2019
田富丰 <40291324+BitgetLimited@users.noreply.github.com>
2021-10-12 11:02:03 +0800

    Create config.go
    增加golang sdk

---
+++ bitget-golang-sdk-api/config/config.go
@@ -0,0 +1,13 @@
+package config
+
+const (
+	//restApi config
+	BaseUrl       = "https://capi.bitget.com"
+	ApiKey        = ""
+	SecretKey     = ""
+	PASSPHRASE    = ""
+	TimeoutSecond = 30
+
+	//websocket config
+	WsUrl = "wss://csocketapi.bitget.com/ws/v1"
+)
