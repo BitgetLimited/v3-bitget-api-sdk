1090f942f5e23b8ec8c82aeef5239f24f5072d34
jidening <ji.dening@upex.co>
2022-02-09 17:38:06 +0800

    delete swap code

--- bitget-python-sdk-api/bitget/consts.py
+++ bitget-python-sdk-api/bitget/consts.py
@@ -18,34 +18,11 @@ POST = "POST"
 DELETE = "DELETE"
 
 # Base Url
-API_URL = 'https://capi.bitget.com'
+API_URL = 'https://api.bitget.com'
 
 # ws Url
 CONTRACT_WS_URL = 'wss://ws.bitgetapi.com/spot/v1/stream'
 
-# ########################################
-# ##############【swap url】###############
-# ########################################
-# time
-SERVER_TIMESTAMP_URL = '/api/swap/v3/market/time'
-# future
-FUTURE_ACCOUNTS = '/api/swap/v3/account/accounts'
-# 获取所有合约的信息
-SWAP_INSTRUMENTS = '/api/swap/v3/market/contracts'
-# 获取全部ticker信息
-SWAP_TICKETS = '/api/swap/v3/market/tickers'
-# ticker信息
-SWAP_SOMEONE_TICKER = '/api/swap/v3/market/ticker'
-#  合约行情
-API_SWAP_V3_ROOT = '/api/swap/v3/market'
-# 合约账户
-API_SWAP_V3_ACCOUNT = '/api/swap/v3/account'
-# 合约订单
-API_OPTION_ORDER = '/api/swap/v3/order'
-# 合约仓位
-API_CONTRACT_ROOT = '/api/swap/v3/position'
-# 合约跟单
-API_TRACE = '/api/swap/v3/trace'
 
 
 # ########################################
