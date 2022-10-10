23bea58ab63955a92d2e460be742c9f55c1fc482
zhang.yunliang@bitget.com <zhang.yunliang@bitget.com>
2022-10-10 11:26:50 +0800

    修改注释

--- bitget-python-sdk-api/bitget/mix/account_api.py
+++ bitget-python-sdk-api/bitget/mix/account_api.py
@@ -9,9 +9,9 @@ class AccountApi(Client):
         Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, first)
 
     '''
-    获取用户 账户信息
-    symbol: 合约交易对
-    marginCoin: 保证金币种
+    Obtain user account information
+    symbol: Contract transaction pair
+    marginCoin: Deposit currency
     :return:
     '''
     def account(self, symbol, marginCoin):
@@ -24,11 +24,11 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    调整杠杆
-    symbol: 合约交易对
-    marginCoin: 保证金币种
-    leverage: 杠杆倍数
-    holdSide: 持仓方向 long 多仓 short 空仓  全仓时可以不传
+    Adjusting lever
+    symbol: Contract transaction pair
+    marginCoin: Deposit currency
+    leverage: Leverage ratio
+    holdSide: In the position direction, long multi position short short short positions can not be transferred in case of full positions
     :return:
     '''
     def leverage(self, symbol, marginCoin, leverage, holdSide=''):
@@ -43,11 +43,11 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    调整保证金
-    symbol: 合约交易对
-    marginCoin: 保证金币种
-    amount: 保证金金额  正数 增加 负数减少
-    holdSide: 持仓方向 long 多仓 short 空仓  全仓时可以不传
+    Adjustment margin
+    symbol: Contract transaction pair
+    marginCoin: Deposit currency
+    amount: Positive increase and negative decrease of deposit amount
+    holdSide: In the position direction, long multi position short short short positions can not be transferred in case of full positions
     :return:
     '''
     def margin(self, symbol, marginCoin, amount, holdSide=''):
@@ -62,10 +62,10 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    调整保证金模式
-    symbol: 合约交易对
-    marginCoin: 保证金币种
-    marginMode: fixed 逐仓  crossed 全仓
+    Adjust margin mode
+    symbol: Contract transaction pair
+    marginCoin: Deposit currency
+    marginMode: Fixed warehouse by warehouse crossed full warehouse
     :return:
     '''
     def margin_mode(self, symbol, marginCoin, marginMode):
@@ -79,10 +79,10 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    设置持仓模式
-    symbol: 合约交易对
-    marginCoin: 保证金币种
-    holdMode: 持仓模式 single_hold 单项持仓  double_hold双向持仓  默认双向
+    Set position mode
+    symbol: Contract transaction pair
+    marginCoin: Deposit currency
+    holdMode: Position mode single_ Hold single position double_ Hold Bidirectional Position Default Bidirectional Position
     :return:
     '''
     def position_mode(self, symbol, marginCoin, holdMode):
@@ -96,12 +96,12 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    查询可开张数
-    symbol: 合约交易对
-    marginCoin: 保证金币种
-    openPrice： 开仓价格
-    openAmount: 开仓额度
-    leverage: 杠杆倍数 默认20
+    Query the number of open sheets
+    symbol: Contract transaction pair
+    marginCoin: Deposit currency
+    openPrice： Opening price
+    openAmount: Opening limit
+    leverage: Default leverage 20
     :return:
     '''
     def open_count(self, symbol, marginCoin, openPrice, openAmount, leverage=20):
@@ -117,8 +117,8 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    获取账户信息列表
-    productType: umcbl(USDT专业合约) dmcbl(混合合约) sumcbl(USDT专业合约模拟盘)  sdmcbl(混合合约模拟盘)
+    Get account information list
+    productType: Umcbl (USDT professional contract) dmcbl (mixed contract) sumcbl (USDT professional contract simulation disk) sdmcbl (mixed contract simulation disk)
     :return:
     '''
     def accounts(self, productType):
@@ -130,7 +130,7 @@ class AccountApi(Client):
             return "pls check args"
 
     '''
-    获取账户流水信息列表
+    Obtain the list of account flow information
     :return:
     '''
     def accountBill(self, symbol,marginCoin,startTime,endTime,lastEndId = '',pageSize=20,next=False):
