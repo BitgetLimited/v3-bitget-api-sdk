b497bc59747e64b3d44ff4c5080a6dedaaa86ce5
jidening <ji.dening@upex.co>
2023-04-04 16:27:15 +0800

    spot plan

--- bitget-python-sdk-api/bitget/consts.py
+++ bitget-python-sdk-api/bitget/consts.py
@@ -34,6 +34,7 @@ SPOT_MARKET_V1_URL = '/api/spot/v1/market'
 SPOT_ACCOUNT_V1_URL = '/api/spot/v1/account'
 SPOT_ORDER_V1_URL = '/api/spot/v1/trade'
 SPOT_WALLET_V1_URL = '/api/spot/v1/wallet'
+SPOT_PLAN_V1_URL = '/api/spot/v1/plan'
 
 # ########################################
 # ##############【mix url】################
