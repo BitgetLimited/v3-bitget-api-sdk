ebc7af836ea5e9dd9301478e989e557591b5a10e
jidening <ji.dening@upex.co>
2023-10-19 14:27:06 +0800

    php sdk

---
+++ bitget-php-sdk-api/example_api.php
@@ -0,0 +1,28 @@
+<?php
+
+require './vendor/autoload.php';
+
+use test\api\MixOrderTest;
+
+date_default_timezone_set('PRC');
+
+test();
+
+function test()
+{
+    $mixOrderTest = new MixOrderTest();
+    $testPlaceOrder = $mixOrderTest->testPlaceOrder();
+    print_r($testPlaceOrder . "\n");
+
+    $testPost = $mixOrderTest->testPost();
+    print_r($testPost . "\n");
+
+    $testGet = $mixOrderTest->testGet();
+    print_r($testGet . "\n");
+
+    $testGet = $mixOrderTest->testGetWithNoParams();
+    print_r($testGet . "\n");
+}
+
+
+
